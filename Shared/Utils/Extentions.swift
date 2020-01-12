0d5014622c294c8baba283c377567f6af7b460d4
Paul Defilippi <allinfo.pd@gmail.com>
2020-01-12 16:28:04 -0500

    added password validation and create user functionality

---
+++ Shared/Utils/Extentions.swift
@@ -0,0 +1,15 @@
+//
+//  Extentions.swift
+//  sneeks-app
+//
+//  Created by Paul Defilippi on 1/12/20.
+//  Copyright © 2020 Paul Defilippi. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
