d75a576b403182c9b4e4f31bddc639b26e252f68
Paul Defilippi <allinfo.pd@gmail.com>
2020-01-13 14:05:41 -0500

    added some custom styling classes

---
+++ Shared/View/RoundedViews.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedViews.swift
+//  sneeks-app
+//
+//  Created by Paul Defilippi on 1/13/20.
+//  Copyright © 2020 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
+
+class RoundedShadowView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+        layer.shadowColor = AppColors.LightBlue.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 3
+    }
+}
+
+class RoundedImageView: UIImageView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
