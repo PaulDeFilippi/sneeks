f311db91c951e7a2f28c9f425c859ff26a7c3ff4
Paul Defilippi <allinfo.pd@gmail.com>
2020-01-13 19:09:12 -0500

    added anonymous login and linking functionality

--- sneeks-app/Controller/RegisterVC.swift
+++ sneeks-app/Controller/RegisterVC.swift
@@ -65,8 +65,10 @@ class RegisterVC: UIViewController {
         
         activityIndicator.startAnimating()
         
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            
+        guard let authUser = Auth.auth().currentUser else { return }
+        
+        let credential = EmailAuthProvider.credential(withEmail: email, password: password)
+        authUser.link(with: credential) { (result, error) in
             if let error = error {
                 debugPrint(error)
                 return
