af00089ea4327a30d240716ad889f7987dc7f293
Paul Defilippi <allinfo.pd@gmail.com>
2020-01-29 18:48:41 -0500

    setup admin home vc through subclassing home vc

---
+++ sneeks-admin/AdminHomeVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  sneeks-admin
+//
+//  Created by Paul Defilippi on 1/8/20.
+//  Copyright © 2020 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class AdminHomeVC: HomeVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        
+        let addCategoryBtn = UIBarButtonItem(title: "Add Category", style: .plain, target: self, action: #selector(addCategory))
+        navigationItem.rightBarButtonItem = addCategoryBtn
+    }
+    
+    @objc func addCategory() {
+        // seg to add cat view
+    }
+
+
+}
+
---
+++ sneeks-app/XIBs/ProductDetailVC.swift
@@ -0,0 +1,58 @@
+//
+//  ProductDetailVC.swift
+//  sneeks-app
+//
+//  Created by Paul Defilippi on 1/27/20.
+//  Copyright © 2020 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailVC: UIViewController {
+
+    // MARK:- Outlets
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var bgView: UIVisualEffectView!
+    
+    var product: Product!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        productTitle.text = product.name
+        productDescription.text = product.productDescription
+        
+        if let url = URL(string: product.imageUrl) {
+            productImg.kf.setImage(with: url)
+        }
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        if let price = formatter.string(from: product.price as NSNumber) {
+            productPrice.text = price
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissProductView))
+        tap.numberOfTapsRequired = 1
+        bgView.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissProductView() {
+        dismiss(animated: true, completion: nil)
+
+    }
+
+    @IBAction func addToCartClicked(_ sender: Any) {
+        // add product to cart
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func dismissProduct(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
