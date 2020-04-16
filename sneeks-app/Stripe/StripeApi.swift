4a39c1f5173230a1fc0dddf8c89ae2a08f787e41
Paul Defilippi <allinfo.pd@gmail.com>
2020-04-16 16:38:46 -0400

    added display payment details and StripeApi class

---
+++ sneeks-app/Stripe/StripeApi.swift
@@ -0,0 +1,37 @@
+//
+//  StripeApi.swift
+//  sneeks-app
+//
+//  Created by Paul Defilippi on 4/16/20.
+//  Copyright © 2020 Paul Defilippi. All rights reserved.
+//
+
+import Foundation
+import Stripe
+import FirebaseFunctions
+
+class _StripeApi: NSObject, STPCustomerEphemeralKeyProvider {
+    
+    func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+        
+        let data = [
+            "stripe_version": apiVersion,
+            "customer_id": UserService.user.stripeId
+        ]
+        
+        Functions.functions().httpsCallable("createEpemeralKey").call(data) { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                completion(nil, error)
+                return
+            }
+            
+            guard let key = result?.data as? [String: Any] else {
+                completion(nil, nil)
+                return
+            }
+            
+            completion(key, nil)
+        }
+    }
+}
